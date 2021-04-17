3dee01d59d34fee2c9ee4b6ecb1c516578aafaef
Luis Chang <luis.chang@outlook.com>
2021-04-17 16:47:14 -0500

    Initial commit

---
+++ app/src/main/java/dev/luischang/semana03_interaccionactivities/ResultActivity.kt
@@ -0,0 +1,25 @@
+package dev.luischang.semana03_interaccionactivities
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+
+class ResultActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_result)
+
+        val message =intent.getStringExtra("param")
+        val textView = findViewById<TextView>(R.id.tvResult)
+        textView.text = message
+        val btnBack: Button = findViewById(R.id.btnBack)
+
+        btnBack.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+
+    }
+}
